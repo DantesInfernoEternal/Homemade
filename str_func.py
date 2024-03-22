2c0936cc3ec555c57351930e74b70b1977ce00eb
DantesInfernoEternal <161636138+DantesInfernoEternal@users.noreply.github.com>
2024-03-22 10:58:52 +0300

    добавлен новая функция и описание

--- str_func.py
+++ str_func.py
@@ -1,5 +1,16 @@
+"""
+Make all letters high
+"""
 def high_word(word):
   return word.upper()
 
+"""
+Make first letters high
+"""
+def title_word():
+  new_word = str(high_word(word))
+  print (new_word.title())
+
 word = input("Введите слово!")
 print (high_word(word))
+title_word()
